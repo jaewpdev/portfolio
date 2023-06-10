6dbb6e6bfd3be96e0daac18f66fab9b06986e05b
Jae Park <jae.park@carney.co>
2023-06-10 10:38:25 -0400

    Containerize layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import classNames from 'classnames';
 
-import './globals.css'
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Jae Park Portfolio',
   description: 'Portfolio of Software Engineer Jae Park',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const classes = classNames(inter.className, 'container mx-auto px-4');
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={classes}>{children}</body>
     </html>
-  )
+  );
 }
